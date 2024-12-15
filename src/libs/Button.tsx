425c67061f41ce90e548686b2571496ae2cbad18
Hyeonjong <nianelo4@gmail.com>
2024-12-16 00:13:14 +0900

    Implement button states

--- src/libs/Button.tsx
+++ src/libs/Button.tsx
@@ -1,5 +1,5 @@
 import type { ComponentProps, ForwardedRef } from 'react';
-import { forwardRef, createElement } from 'react';
+import { forwardRef, createElement, useRef, useState } from 'react';
 
 type DynamicComponentProps<D extends keyof JSX.IntrinsicElements, T> = {
   as?: T;
@@ -10,7 +10,7 @@ type DynamicComponentProps<D extends keyof JSX.IntrinsicElements, T> = {
     : ComponentProps<D>);
 
 type ButtonRootVariants = {
-  //
+  disabled?: boolean;
 };
 
 type ButtonRootProps<T> = Omit<
@@ -22,15 +22,99 @@ type ButtonRootProps<T> = Omit<
 function ButtonRoot<
   T extends keyof JSX.IntrinsicElements | undefined = undefined,
 >(
-  { as = 'button', children, ...otherProps }: ButtonRootProps<T>,
+  {
+    as = 'button',
+    disabled = false,
+    onMouseEnter,
+    onMouseLeave,
+    onMouseDown,
+    onMouseUp,
+    onKeyDown,
+    onKeyUp,
+    onFocus,
+    onBlur,
+    children,
+    ...otherProps
+  }: ButtonRootProps<T>,
   ref: ForwardedRef<unknown>,
 ) {
+  const lastInteractionRef = useRef<'keyboard' | 'mouse'>('keyboard');
+  const [isHover, setIsHover] = useState(false);
+  const [isFocus, setIsFocus] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
   return createElement(
     as,
     {
       ref,
       type: 'button',
+      disabled,
+      onMouseEnter: (e: any) => {
+        onMouseEnter?.(e);
+        if (!disabled) {
+          setIsHover(true);
+        }
+      },
+      onMouseLeave: (e: any) => {
+        onMouseLeave?.(e);
+        if (!disabled) {
+          setIsHover(false);
+        }
+      },
+      onMouseDown: (e: any) => {
+        onMouseDown?.(e);
+        if (!disabled) {
+          setIsFocus(false);
+          setIsActive(true);
+        }
+        lastInteractionRef.current = 'mouse';
+      },
+      onMouseUp: (e: any) => {
+        onMouseUp?.(e);
+        if (!disabled) {
+          setIsActive(false);
+        }
+      },
+      onKeyDown: (e: any) => {
+        onKeyDown?.(e);
+        if (!disabled) {
+          setIsActive(e.code === 'Space');
+        }
+        lastInteractionRef.current = 'keyboard';
+      },
+      onKeyUp: (e: any) => {
+        onKeyUp?.(e);
+        if (!disabled) {
+          setIsActive(false);
+        }
+      },
+      onFocus: (e: any) => {
+        onFocus?.(e);
+        if (!disabled) {
+          setIsFocus(lastInteractionRef.current === 'keyboard');
+        }
+      },
+      onBlur: (e: any) => {
+        onBlur?.(e);
+        if (!disabled) {
+          setIsFocus(false);
+        }
+        lastInteractionRef.current = 'keyboard';
+      },
       ...otherProps,
+      'data-state': disabled
+        ? 'disabled'
+        : [
+            isHover ? 'hover' : undefined,
+            isFocus ? 'focus' : undefined,
+            isActive ? 'active' : undefined,
+          ]
+            .filter(Boolean)
+            .join(' '),
+      'data-hover': isHover ? '' : undefined,
+      'data-focus': isFocus ? '' : undefined,
+      'data-active': isActive ? '' : undefined,
+      'data-disabled': disabled ? '' : undefined,
     },
     children,
   );
